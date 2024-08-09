427eef73694ef6e89489fb0e79b0e0f9775245bd
andresrib <andresrib3@gmail.com>
2024-08-08 21:56:33 -0300

    feat task

--- task.py
+++ task.py
@@ -1,10 +1,10 @@
 from CustomSelenium import CustomSelenium
+from robocorp.tasks import task
 
+@task
 def yahoo_search():
     selenium = CustomSelenium()
     selenium.set_web_driver()
     selenium.open_url("https://news.yahoo.com/")
     selenium.search("magic, the gathering")
 
-if __name__ == "__main__":
-    yahoo_search()
